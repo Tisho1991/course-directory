f87d88646d2c633358768f019baa57c09a9ddf6b
Tisho1991 <tihomir.bogoslovov@adentis.pt>
2021-04-01 19:07:06 +0100

    Updates

---
+++ precache-manifest.13b9ae8a2a60490db2f20ec7d3b6f082.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f239ec2c872050f3e53",
+    "url": "/course-directory/static/css/main.245f2e85.chunk.css"
+  },
+  {
+    "revision": "6f239ec2c872050f3e53",
+    "url": "/course-directory/static/js/main.6f239ec2.chunk.js"
+  },
+  {
+    "revision": "25a97381f2fd20d18f35",
+    "url": "/course-directory/static/js/1.25a97381.chunk.js"
+  },
+  {
+    "revision": "527751acb6022af06d5d",
+    "url": "/course-directory/static/js/runtime~main.527751ac.js"
+  },
+  {
+    "revision": "87301d9e5d845fff5bd37fb2099488e3",
+    "url": "/course-directory/index.html"
+  }
+];
